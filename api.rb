1d387dc4ef4fda3bd87ec48fc723c1905bd8183c
Matheus Ashton Silva <matheusashton@gmail.com>
2013-10-04 15:25:38 -0300

    some refactoring, add user to a group in POST /groups and removing repo @ DELETE /repo

--- api.rb
+++ api.rb
@@ -5,44 +5,55 @@ require 'sinatra/respond_with'
 require 'json'
 
 config_file 'config.yml'
-repo_config = nil
+DELETED_STATUS = 204
+CREATED_STATUS = 201
 
 before do
-  repo_config = RepoConfig.new(settings.gitolite_root_dir)
+  @repo_config = RepoConfig.new(settings.gitolite_root_dir)
 end
 
 get '/repos' do
   respond_to do |f|
-    f.json { JSON.dump(repo_config.repos) }
+    f.json { JSON.dump(@repo_config.repos) }
   end
 end
 
 post '/repos' do
-  repo_config.add_repo params[:repo_name]
+  @repo_config.add_repo params[:repo_name]
 
-  201
+  CREATED_STATUS
+end
+
+delete '/repos' do
+  @repo_config.remove_repo params[:repo_name]
+
+  DELETED_STATUS
 end
 
 get '/users' do
   respond_to do |f|
-    f.json { JSON.dump(repo_config.users) }
+    f.json { JSON.dump(@repo_config.users) }
   end
 end
 
 post '/users' do
-  repo_config.add_user params[:username], params[:ssh_key]
+  @repo_config.add_user params[:username], params[:ssh_key]
 
-  201
+  CREATED_STATUS
 end
 
 get '/groups' do
   respond_to do |f|
-    f.json { JSON.dump(repo_config.groups) }
+    f.json { JSON.dump(@repo_config.groups) }
   end
 end
 
 post '/groups' do
-  repo_config.add_group params[:group_name]
+  if params[:username]
+    @repo_config.add_group params[:group_name], params[:username]
+  else
+    @repo_config.add_group params[:group_name]
+  end
 
-  201
+  CREATED_STATUS
 end
--- spec/api/api_spec.rb
+++ spec/api/api_spec.rb
@@ -1,57 +1,96 @@
 require_relative '../spec_helper'
+require 'pry'
 
 describe Sinatra::Application do
+  let(:repo_config_double) { double() }
+  let(:repo_name) { 'test' }
+  let(:username) { 'user' }
+  let(:ssh_key) { 'ssh_key as string' }
+  let(:group_name) { 'group' }
+  let(:list) { Array.new }
+  let(:hash) { Hash.new }
+  let(:created_http_status) { 201 }
+  let(:ok_http_status) { 200 }
+  let(:deleted_http_status) { 204 }
+
+  before do
+    stub_const 'RepoConfig', repo_config_double
+
+    repo_config_double.stub(:new).and_return(repo_config_double)
+  end
+
   context "responding to GET /repos" do
 
     it "should return the list of repositories" do
-      get '/repos'
+      repo_config_double.should_receive(:repos).and_return(list)
 
-      last_response.status.should be_eql 200
+      get '/repos'
+      last_response.status.should be_eql ok_http_status
       JSON.parse(last_response.body).should be_an_instance_of Array
     end
   end
 
   context "responding to POST /repos" do
     it "should create the repo" do
-      post '/repos', :repo_name => "test"
+      repo_config_double.should_receive(:add_repo).with(repo_name)
 
-      last_response.status.should be_eql 201
+      post '/repos', :repo_name => repo_name
+      last_response.status.should be_eql created_http_status
+    end
+  end
+
+  context "responding to DELETE /repos" do
+    it "should delete the repository" do
+      repo_config_double.should_receive(:remove_repo).with(repo_name)
+
+      delete '/repos', :repo_name => repo_name
+      last_response.status.should be_eql deleted_http_status
     end
   end
 
   context "responding to GET /users" do
 
     it "should return the list of users" do
-      get '/users'
+      repo_config_double.should_receive(:users).and_return(list)
 
-      last_response.status.should be_eql 200
+      get '/users'
+      last_response.status.should be_eql ok_http_status
       JSON.parse(last_response.body).should be_an_instance_of Array
     end
   end
 
   context "responding to POST /users" do
     it "should create the user" do
-      post '/users', :username => "test", :ssh_key => "ssh key as string"
+      repo_config_double.should_receive(:add_user).with(username, ssh_key)
 
-      last_response.status.should be_eql 201
+      post '/users', :username => username, :ssh_key => ssh_key
+      last_response.status.should be_eql created_http_status
     end
   end
 
   context "responding to GET /groups" do
-
     it "should return the list of groups" do
-      get '/groups'
+      repo_config_double.should_receive(:groups).and_return(hash)
 
-      last_response.status.should be_eql 200
+      get '/groups'
+      last_response.status.should be_eql ok_http_status
       JSON.parse(last_response.body).should be_an_instance_of Hash
     end
   end
 
   context "responding to POST /groups" do
     it "should create the group" do
-      post '/groups', :group_name => "test"
+      repo_config_double.should_receive(:add_group).with(group_name)
 
-      last_response.status.should be_eql 201
+      post '/groups', :group_name => group_name
+      last_response.status.should be_eql created_http_status
+    end
+
+    it "should create the group and add users" do
+      repo_config_double.should_receive(:add_group).with(group_name, username)
+
+      post '/groups', :group_name => group_name, :username => username
+      last_response.status.should be_eql created_http_status
     end
   end
 end
